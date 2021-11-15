3ea2e5e9e2a5ab25f72916a1ad672c021bb73c4a
Adam <a.6942@yahoo.com>
2021-11-14 23:29:32 -0500

    added function to view employee by department

--- server.js
+++ server.js
@@ -428,3 +428,19 @@ updateEmployee = () => {
       });
     });
   };
+
+employeeDepartment = () => {
+    console.log('Showing employee by departments...');
+    const sql = `SELECT employee.first_name, 
+                        employee.last_name, 
+                        department.name AS department
+                 FROM employee 
+                 LEFT JOIN role ON employee.role_id = role.id 
+                 LEFT JOIN department ON role.department_id = department.id`;
+  
+    connection.promise().query(sql, (err, rows) => {
+      if (err) throw err; 
+      console.table(rows); 
+      promptUser();
+    });          
+  };
